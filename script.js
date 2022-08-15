2b1087bd1830e54cbf0c1a35b7ea47467333d0f9
ruanmolinari98 <ruanmolinari98@gmail.com>
2022-08-15 19:47:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+if(idade>18){
+    console.log('sua idade é maior que 18');
+}
+
+//2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
+
+if(idade>18 && humana==true){
+    console.log('você é humana e é maior de idade!')
+}
+
+//3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+
+if(aniversario == jan || aniversario == dez){
+    console.log('você faz aniversário em'+ ' '+aniversario);
+}
